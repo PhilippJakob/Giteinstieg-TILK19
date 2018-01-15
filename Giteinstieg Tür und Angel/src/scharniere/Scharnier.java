bb59976e5b8051feefd5176b804e7e245baab7cc
Jakob <Jakob@PC2301L01.osz.intern>
2018-01-15 14:50:56 +0100

    Methode bohren aus Türrahmensicht

--- Giteinstieg Tür und Angel/src/scharniere/Scharnier.java
+++ Giteinstieg Tür und Angel/src/scharniere/Scharnier.java
@@ -2,9 +2,12 @@ package scharniere;
 
 public class Scharnier {
 
-
-
-
 	//Im Tührrahmen ergänzt
 	int bohrlöcherAnzahl = 4;
+
+	public void bohren()
+	{
+		//stammt aus dem Türrahmen
+		bohrlöcherAnzahl = 5;
+	}
 }
