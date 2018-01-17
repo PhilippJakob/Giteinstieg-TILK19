f9af17b1e4fa8234b0c429c28a6fc3541af973da
Jonas N. Henle <Jonas N. Henle@DESKTOP-SNSV>
2018-01-17 14:15:00 +0100

    Hinzufügen eines Kombinationsschloßes.

--- Giteinstieg Tür und Angel/src/schlösser/kombinationsschloss.java
+++ Giteinstieg Tür und Angel/src/schlösser/kombinationsschloss.java
@@ -1,5 +1,31 @@
 package schlösser;
 
-public class kombinationsschloss {
+public class kombinationsschloss
+{
+	public int bolzenbreite = 20;
+	public boolean schließzustand = false;
+	public int skombinationStelleEins = 4;
+	public int skombinationStelleZwei = 0;
+	public int skombinationStelleDrei = 4;
+
+	public int gkombinationStelleEins = 9;
+	public int gkombinationStelleZwei = 9;
+	public int gkombinationStelleDrei = 9;
+
+	public void kontrollierenKombination()
+	{
+		if(gkombinationStelleEins == skombinationStelleEins)
+		{
+			System.out.println("Klick");
+			if(gkombinationStelleZwei == skombinationStelleZwei)
+			{
+				System.out.println("Klick");
+				if(gkombinationStelleDrei == skombinationStelleEins)
+				{
+					System.out.println("Klick, geöffnet!");
+				}
+			}
+		}
+	}
 
 }
