b5921966934e2341a34d84d6b4a45f731ffae687
markeffsky.lukas <markeffsky.lukas@PC230102.osz.intern>
2018-01-17 12:30:48 +0100

    Fenster anzeigen

--- Giteinstieg Tür und Angel/src/application/Main.java
+++ Giteinstieg Tür und Angel/src/application/Main.java
@@ -1,17 +1,16 @@
 package application;
-	
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(FXMLLoader.load(getClass().getResource("View.fxml")));
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
@@ -19,7 +18,7 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
