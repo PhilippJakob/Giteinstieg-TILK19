e3a023e7d06b0b58e4fcd7a508a572f8497a1d89
woehrle.patrick <woehrle.patrick@PC230113.osz.intern>
2018-01-17 13:06:35 +0100

    Kommentarzeile hinzugefügt (Patricks Testversuch

--- Giteinstieg Tür und Angel/src/türen/Tür.java
+++ Giteinstieg Tür und Angel/src/türen/Tür.java
@@ -1,13 +1,15 @@
+//Patricks Testversuch
+
 package türen;
 
 public class Tür
 {
 	int anzahlScharniere = 2;
-	
+
 	public void methodeFürConflictJakob1()
 	{}
 
 	public void anbringenSchloßblende()
 	{}
-	
+
 }
