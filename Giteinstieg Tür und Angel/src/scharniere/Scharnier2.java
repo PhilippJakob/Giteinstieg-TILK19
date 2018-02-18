a3136da42ce7220eefed413c432f746e0d82a931
Steffen <Steffen@Naasner2.fritz.box>
2018-02-18 14:51:33 +0100

    ...

--- Giteinstieg Tür und Angel/src/scharniere/Scharnier2.java
+++ Giteinstieg Tür und Angel/src/scharniere/Scharnier2.java
@@ -1,6 +1,11 @@
-package scharniere;
-
+/*
+ * meine Scharniere
+ */
+ package scharniere;
 
 public class Scharnier2 {
 
+	public void scharnier2(){
+		System.out.println("neues Scharnier");
+	}
 }
