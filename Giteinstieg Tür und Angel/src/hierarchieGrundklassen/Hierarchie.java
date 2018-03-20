db6da7b00cf0152b3272719d1e5f23fbf395eb55
Philipp Jakob <jakob@osztiem.de>
2018-03-20 18:50:14 +0100

    HierarchieReviews Branch nur für funktionierende Review Stände 1. Review 21.03.2018 Hierarchien zwischen Vorurteilen werden vollständig angezeigt
    Signed-off-by: Philipp Jakob <jakob@osztiem.de>

--- Giteinstieg Tür und Angel/src/hierarchieGrundklassen/Hierarchie.java
+++ Giteinstieg Tür und Angel/src/hierarchieGrundklassen/Hierarchie.java
@@ -1,5 +1,11 @@
 package hierarchieGrundklassen;
 
+/**
+ *
+ * @author PArbeit
+ *  Abgenommene Funktionalität: Abnahmeprotokoll unter AbnahmeprotokollHierarchie210318.docx im Poolverzeichnis
+ *
+ */
 public class Hierarchie {
 
 }
